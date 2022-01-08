e793decbab1a374a64e6a65e0e7ce305f768beb2
lucianovsjr <lucianovsjr@gmail.com>
2022-01-08 13:40:40 -0300

    use Tabs to Create and Edit views

--- src/resource/Product/Edit.js
+++ src/resource/Product/Edit.js
@@ -1,7 +1,8 @@
 import React from "react";
 import {
     Edit,
-    SimpleForm,
+    TabbedForm,
+    FormTab,
     TextInput,
     BooleanInput,
     ReferenceInput,
@@ -25,14 +26,16 @@ const ProductEdit = props => {
     
     return (
         <Edit onSuccess={onSuccess} mutationMode="pessimistic" {...props}>
-            <SimpleForm redirect="list" validate={validation.validationProduct}>
-                <ReferenceInput source="product_type" reference="product_type" required validate={validation.validateProductType}>
-                    <SelectInput optionText="name" />
-                </ReferenceInput>
-                <TextInput source="description" className={classes.inputXLg} required validate={validation.validateDescription} />
-                <TextInput source="full_description" multiline minRows={3} maxRows={5} className={classes.inputXLg} />
-                <BooleanInput source="active" />
-            </SimpleForm>
+            <TabbedForm redirect="list" validate={validation.validationProduct}>
+                <FormTab label="resources.product.tabs.general">
+                    <ReferenceInput source="product_type" reference="product_type" required validate={validation.validateProductType}>
+                        <SelectInput optionText="name" />
+                    </ReferenceInput>
+                    <TextInput source="description" className={classes.inputXLg} required validate={validation.validateDescription} />
+                    <TextInput source="full_description" multiline minRows={3} maxRows={5} className={classes.inputXLg} />
+                    <BooleanInput source="active" />
+                </FormTab>
+            </TabbedForm>
         </Edit>
     );
 }
