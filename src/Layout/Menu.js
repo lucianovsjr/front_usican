2789c8e453562bbbd7a8c981d3d8d9e11fc907e2
lucianovsjr <lucianovsjr@gmail.com>
2022-02-16 22:56:37 -0300

    Add expand menu list

--- src/Layout/Menu.js
+++ src/Layout/Menu.js
@@ -1,36 +1,48 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import { Menu as raMenu, MenuItemLink, getResources, useTranslate } from 'react-admin';
 
 import { useSelector } from 'react-redux';
 import get from 'lodash/get';
 
 import makeStyles from '@material-ui/styles/makeStyles'
-import MenuItem from '@material-ui/core/MenuItem';
 import DefaultIcon from '@material-ui/icons/ViewList';
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
+import StorageIcon from '@material-ui/icons/Storage';
+
+import SubMenu from './SubMenu'
 
 const MODULES = [
-    { key: 'inventory', name: 'Estoque' },
-    { key: 'sales', name: 'Vendas' }
+    { key: 'inventory', name: 'Estoque', icon: StorageIcon },
+    { key: 'sales', name: 'Vendas', icon: BusinessCenterIcon }
 ]
 
-const useStyles = makeStyles({
-    rootMenu: {
-        marginTop: '1.5em',
-    },
-    titleMenuItem: {
-        fontSize: 18,
-    },
-});
+const useStyles = makeStyles(theme => ({
+        rootMenu: {
+            marginTop: '1em',
+        },
+        rootItem: {
+            borderLeft: `${theme.spacing.unit / 2}px solid ${theme.palette.common.white}`,
+        },
+        activeItem: {
+            borderLeft: `${theme.spacing.unit / 2}px solid ${theme.palette.primary.main}`,
+            color: theme.palette.text.primary,
+        }
+    })
+);
 
-const ModuleMenu = ({items, title, open, ...rest}) => {
+const ModuleMenu = ({items, keyModule, title, Icon, open, subMenuIsOpen, handleToggle, dense}) => {
     const classes = useStyles();
     const translate = useTranslate()
 
     return (
-        <React.Fragment>
-            <MenuItem button={false} classes={{ root: classes.titleMenuItem }} {...rest}>
-                {title}
-            </MenuItem>
+        <SubMenu
+            handleToggle={() => handleToggle(keyModule)}
+            isOpen={subMenuIsOpen}
+            name={title}
+            icon={<Icon />}
+            dense={dense}
+        >
             {items.map(item => item.hasList && (
                 <MenuItemLink
                     key={item.name}
@@ -38,21 +50,36 @@ const ModuleMenu = ({items, title, open, ...rest}) => {
                             pathname: `/${item.name}`,
                             state: { _scrollToTop: true },
                         }}
-                    primaryText={item.options.label || translate(`resources.${item.name}.name`, { smart_count: 1 })}
+                    primaryText={item.options.label || translate(`resources.${item.name}.name`, { smart_count: 2 })}
                     leftIcon={item.icon ? <item.icon /> : <DefaultIcon />}
                     sidebarIsOpen={open}
+                    className={classes.rootItem}
+                    activeClassName={classes.activeItem}
                 />
             ))}
-        </React.Fragment>
+        </SubMenu>
     );
 };
 
 const Menu = (props) => {
+    const { dense = false } = props;
     const classes = useStyles();
     const resources = useSelector(getResources);
     const open = useSelector(state => state.admin.ui.sidebarOpen)
 
     const moduleResources = MODULES.map(module => resources.filter(resource => get(resource, 'options.module') === module.key))
+
+    const [openMenus, setOpenMenus] = useState({});
+
+    useEffect(() => {
+        const menus = {};
+        MODULES.forEach(module => menus[module.key] = true)
+        setOpenMenus(menus)
+    }, [])
+
+    const handleToggle = (menu) => {
+        setOpenMenus(state => ({ ...state, [menu]: !state[menu] }));
+    };
 
     return (
         <raMenu className={classes.rootMenu} {...props}>
@@ -63,8 +90,13 @@ const Menu = (props) => {
                             && (
                                 <ModuleMenu
                                     items={moduleResources[i]}
+                                    keyModule={module.key}
                                     title={module.name}
+                                    Icon={module.icon}
                                     open={open}
+                                    subMenuIsOpen={openMenus[module.key]}
+                                    handleToggle={handleToggle}
+                                    dense={dense}
                                 />
                             )
                 )
